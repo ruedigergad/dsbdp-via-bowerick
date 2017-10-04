384313a16863019b3ab0a37f02f35f61be0a73bc
Ruediger Gad <r.c.g@gmx.de>
2017-10-04 08:54:11 +0200

    Add bowerick and dsbdp dependencies.

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-assorted-utils "1.18.2"]
                  [org.clojure/tools.cli "0.3.3"]
-                 ]
+                 [dsbdp "0.4.0"]
+                 [bowerick "2.1.1"]]
   :global-vars {*warn-on-reflection* true}
   :profiles  {:repl  {:dependencies  [[jonase/eastwood "0.2.2" :exclusions  [org.clojure/clojure]]]}}
   :plugins [[lein-cloverage "1.0.6"]]
